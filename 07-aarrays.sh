8096b24c68212980c30c67bf106c77f6e0627181
vishnusaib <sri.vishnusai9@gmail.com>
2025-05-31 22:47:11 +0530

    hello world started

--- 07-aarrays.sh
+++ 07-aarrays.sh
@@ -2,3 +2,7 @@
 
 Movies=("Inception" "The Matrix" "Interstellar" "The Godfather")
 echo "Movies in the array:${Movies[0]}"
+# To print a specific element in the array, use the index as shown above.
+
+echo "All movies in the array : ${Movies[@]}"
+# To print all elements in the array, use the @ symbol."
