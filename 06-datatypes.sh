ab21e398c881110dc0bbee38b666d1bb30abbc5b
vishnusaib <sri.vishnusai9@gmail.com>
2025-05-31 22:37:21 +0530

    hello world started

---
+++ 06-datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script demonstrates the use of datatypes in shell scripting
+
+Number=200
+Number2=300
+
+TimeStamp=$(date)
+
+echo "This code is executed at $TimeStamp"
+
+#Sum = $((Number + Number2))
