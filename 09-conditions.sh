ecc3dad22cc18d3a582155c88b7b84d71cd9bdb4
vishnusaib <sri.vishnusai9@gmail.com>
2025-06-01 13:55:15 +0530

    hello world started

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script demonstrates the use of If conditional statements in bash.
+echo "Enter a number: "
+read $num
+
+if [$num -lt 100];
+    then
+    echo "Entered number is less than 100"
+else
+    echo "Entered number is greater than 100"
+fi
+# This script demonstrates the use of If conditional statements in bash.
