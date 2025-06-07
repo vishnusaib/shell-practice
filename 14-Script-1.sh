9fb5c7d8f9072225feb4038896191b1a88f36b3d
vishnusaib <sri.vishnusai9@gmail.com>
2025-06-07 14:55:24 +0530

    shell-practice

---
+++ 14-Script-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GREETINGS="Hello! Good Morning"
+
+echo "$GREETINGS"
+
+echo "PID of Script-1: $$"
+
+./Script-2.sh
---
+++ 15-Script-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "PID of Script-1: $$"
+
+echo "$GREETINGS"
+
+# Script-1.sh Script-2.sh
