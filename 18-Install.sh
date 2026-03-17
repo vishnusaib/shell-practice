035f0a456a1ee591cc400a1072ba539099efca53
vishnusaib <sri.vishnusai9@gmail.com>
2026-03-17 20:02:07 +0530

    special vars

--- 18-Install.sh
+++ 18-Install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# you run this file as sudo before running in shell 
 userid=$(id -u)
 
 if [ $userid -ne 0 ]
 then
     echo "your not root user"
+    exit 1
 else
     echo "Hi Root User"
 fi
